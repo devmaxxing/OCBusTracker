d4d4a873879c2c55e74df4d6c1d0ba7869f17e8d
oliverfei <ofei006@uottawa.ca>
2016-07-16 10:06:33 -0400

    Add project files.

---
+++ OC Bus Tracker/MainPage.xaml.cs
@@ -0,0 +1,443 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.Web.Http;
+using Newtonsoft.Json.Linq;
+using Windows.UI.Popups;
+using Windows.Networking.Connectivity;
+using SQLite.Net.Attributes;
+using Windows.UI.Xaml;
+using Windows.UI.Core;
+//using System.Diagnostics;
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace OC_Bus_Tracker
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public HttpClient httpClient = new HttpClient();
+        public ObservableCollection<RouteInfo> routesList = new ObservableCollection<RouteInfo>();
+        public ObservableCollection<RouteInfo> savedRoutes = new ObservableCollection<RouteInfo>();
+        public ObservableCollection<string> stopNames = new ObservableCollection<string>();
+        public ObservableCollection<string> filtered = new ObservableCollection<string>();
+        public SQLite.Net.SQLiteConnection conn;
+        public string httpQueryParams = "/?appID=" + OCTranspoInfo.APP_ID + "&apiKey=" + OCTranspoInfo.API_KEY + "&format=json";
+        public string path;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            loadStops();
+
+            SearchBox.ItemsSource = filtered;
+            resultsList.DataContext = routesList;
+            pinnedList.DataContext = savedRoutes;
+
+            path = System.IO.Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path,
+            "db.sqlite");
+            conn = new SQLite.Net.SQLiteConnection(new
+               SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+            conn.CreateTable<StopRoute>();
+        }
+
+        private async Task<string> loadStops()
+        {
+            var uri = new System.Uri("ms-appx:///stops.json");
+            var stopsFile = await Windows.Storage.StorageFile.GetFileFromApplicationUriAsync(uri);
+            var stopsText = await Windows.Storage.FileIO.ReadTextAsync(stopsFile);
+            JArray stops = (JArray)JArray.Parse(stopsText);
+            foreach(var stop in stops)
+            {
+                stopNames.Add(stop["stop_code"] + " " + stop["stop_name"]);
+            }
+            return "success";
+        }
+
+        private async void loadPinned()
+        {
+            var query = conn.Table<StopRoute>();
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                () =>
+                {
+                    savedRoutes.Clear();
+                }
+                );
+            SearchRingPinned.IsActive = true;
+            SearchRingPinned.Visibility = Visibility.Visible;
+            foreach (var stopRoute in query)
+            {
+                if (IsInternet())
+                {
+
+                    string stopName = stopRoute.stopName;
+                    string routeName = stopRoute.routeName;
+                    int directionId = stopRoute.directionId;
+                    string stopId = stopRoute.stopId;
+                    string routeNum = stopRoute.routeNum;
+                    var tripTimes = new string[3] { "No upcoming trips", "", "" };
+
+                    HttpRequestMessage stopRouteRequest = new HttpRequestMessage();
+                    var queryString = httpQueryParams + "&stopNo=" + stopId + "&routeNo=" + routeNum;
+                    stopRouteRequest.RequestUri = new Uri(OCTranspoInfo.ROUTE_URL + queryString);
+
+                    var response = await getJSON(stopRouteRequest);
+                    JObject routeInfo = (JObject)JObject.Parse(response)["GetNextTripsForStopResult"];
+                    
+                    if ((string)routeInfo["Error"] == "")
+                    {
+                        JObject route;
+                        try
+                        {
+                            route = (JObject)routeInfo["Route"]["RouteDirection"];
+                        }
+                        catch (Exception e)
+                        {
+                            route = (JObject)routeInfo["Route"]["RouteDirection"][directionId];
+                        }
+
+                        JArray trips = new JArray();
+                        if (route["Trips"].HasValues)
+                        {
+                            try
+                            {
+                                trips = (JArray)route["Trips"];
+                            }
+                            catch (Exception e)
+                            {
+                                try
+                                {
+                                    trips = (JArray)route["Trips"]["Trip"];
+                                }
+                                catch (Exception e2)
+                                {
+                                    trips.Add((JObject)route["Trips"]);
+                                }
+                            }
+                        }
+
+                        var numTrips = trips.Count();
+                        for (var j = 0; j < numTrips; j++)
+                        {
+                            tripTimes[j] = (string)trips[j]["AdjustedScheduleTime"] + " min";
+                        }
+                    }
+                   
+                    RouteInfo r = new RouteInfo(stopName, routeName, directionId, tripTimes[0], tripTimes[1], tripTimes[2]);
+                    r.StopId = stopId;
+                    r.RouteNum = routeNum;
+
+                    await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                    () =>{
+                        savedRoutes.Add(r);
+                    });
+                }else
+                {
+                    MessageDialog msgDialog = new MessageDialog("No internet connection.", "Error");
+                    await msgDialog.ShowAsync();
+                }
+                
+            }
+            SearchRingPinned.IsActive = false;
+            SearchRingPinned.Visibility = Visibility.Collapsed;
+        }
+
+        private async void SearchStop(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            if (IsInternet())
+            {
+                SearchRing.IsActive = true;
+                SearchRing.Visibility = Visibility.Visible;
+
+                HttpRequestMessage stopRequest = new HttpRequestMessage();
+                var queryString = httpQueryParams + "&stopNo=" + sender.Text.Split(' ')[0];
+                stopRequest.RequestUri = new Uri(OCTranspoInfo.STOP_URL + queryString);
+
+                var response = await getJSON(stopRequest);
+                JObject routeInfo = (JObject)JObject.Parse(response)["GetRouteSummaryForStopResult"];
+                SearchRing.IsActive = false;
+                SearchRing.Visibility = Visibility.Collapsed;
+                if ((string)routeInfo["Error"] == "")
+                {
+                    addRouteInfo(routesList, routeInfo);
+                }
+                else
+                {
+                    MessageDialog msgDialog = new MessageDialog("Sorry, we were unable to get the requested stop info.", "Error");
+                    await msgDialog.ShowAsync();
+                }
+            }
+            else
+            {
+                MessageDialog msgDialog = new MessageDialog("No internet connection.", "Error");
+                await msgDialog.ShowAsync();
+            }
+                        
+        }
+
+        private async void addRouteInfo (ObservableCollection<RouteInfo> collection, JObject routeInfo)
+        {
+            JArray routes = new JArray();
+            try
+            {
+                routes = (JArray)routeInfo["Routes"]["Route"];
+            }catch(ArgumentException e1)
+            {
+                routes = (JArray)routeInfo["Routes"];
+            }catch(Exception e)
+            {
+                routes.Add((JObject)routeInfo["Routes"]["Route"]);
+            }
+
+            collection.Clear();
+            var stopName = (string)routeInfo["StopDescription"];
+            var stopId = (string)routeInfo["StopNo"];
+            var numRoutes = routes.Count();
+
+            if(numRoutes == 0)
+            {
+                MessageDialog msgDialog = new MessageDialog("There are no upcoming trips at this stop.", "Message");
+                await msgDialog.ShowAsync();
+            }
+
+            for (var i = 0; i < numRoutes; i++)
+            {
+                var route = routes[i];
+                var tripTimes = new string[3] { "", "", "" };
+                
+                var routeName = (string)route["RouteNo"] + " " + (string)route["RouteHeading"];
+                var routeNum = (string) route["RouteNo"];
+                var directionId = 0;
+                try
+                {
+                    directionId = (int)route["DirectionID"];
+                }
+                catch (Exception e) { }
+
+                JArray trips;
+                try
+                {
+                    trips = (JArray)route["Trips"];
+                }
+                catch (Exception e)
+                {
+                    try
+                    {
+                        trips = (JArray)route["Trips"]["Trip"];
+                    }catch(Exception e2)
+                    {
+                        trips = new JArray();
+                        trips.Add((JObject)route["Trips"]);
+                    }                    
+                }
+
+                if (trips != null)
+                {
+                    var numTrips = trips.Count();
+                    if (numTrips > 0)
+                    {
+                        for (var j = 0; j < numTrips; j++)
+                        {
+                            tripTimes[j] = (string)trips[j]["AdjustedScheduleTime"] + " min";
+                        }
+                        RouteInfo r = new RouteInfo(stopName, routeName, directionId, tripTimes[0], tripTimes[1], tripTimes[2]);
+                        r.StopId = stopId;
+                        r.RouteNum = routeNum;
+                        collection.Add(r);
+                    }
+                }                
+            }
+        }
+
+        private bool IsInternet()
+        {
+            ConnectionProfile connections = NetworkInformation.GetInternetConnectionProfile();
+            bool internet = connections != null && connections.GetNetworkConnectivityLevel() == NetworkConnectivityLevel.InternetAccess;
+            return internet;
+        }
+
+        private async Task<String> getJSON(HttpRequestMessage message)
+        {
+            HttpResponseMessage response = await httpClient.SendRequestAsync(message);
+            return response.Content.ToString();
+        }
+
+        public class StopRoute
+        {
+            [PrimaryKey, AutoIncrement]
+            public int id { get; set; }
+            public string stopId { get; set; }
+            public string stopName { get; set; }
+            public string routeNum { get; set; }
+            public string routeName { get; set; }
+            public int directionId { get; set; }
+        }
+
+        public class RouteInfo
+        {
+            public RouteInfo(string routeName)
+            {
+                RouteName = routeName;
+            }
+
+            public RouteInfo(string routeName, string t1, string t2, string t3)
+            {
+                RouteName = routeName;
+                RouteTime1 = t1;
+                RouteTime2 = t2;
+                RouteTime3 = t3;
+            }
+
+            public RouteInfo(string stopName, string routeName, string t1, string t2, string t3)
+                :this (routeName,t1,t2,t3)
+            {
+                StopName = stopName;
+            }
+
+            public RouteInfo(string stopName, string routeName, int directionId, string t1, string t2, string t3)
+                :this (stopName, routeName, t1, t2, t3)
+            {
+                DirectionId = directionId;
+            }
+
+            public string RouteName { get; set; }
+            public string RouteTime1 { get; set; }
+            public string RouteTime2 { get; set; }
+            public string RouteTime3 { get; set; }
+            public string StopName { get; set; }
+            public string StopId { get; set; }
+            public string RouteNum { get; set; }
+            public int DirectionId { get; set; }
+        }
+
+        private void PinRoutes(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = resultsList.SelectedItems.OfType<RouteInfo>();
+            foreach(var item in selectedItems)
+            {
+                if (savedRoutes.Where(route =>
+                route.StopId == item.StopId && route.RouteNum == item.RouteNum &&
+                route.DirectionId == item.DirectionId).Count() == 0)
+                {
+                    var s = conn.Insert(new StopRoute()
+                    {
+                        stopName = item.StopName,
+                        directionId = item.DirectionId,
+                        stopId = item.StopId,
+                        routeNum = item.RouteNum,
+                        routeName = item.RouteName
+                    });
+
+                    savedRoutes.Add(item);
+                }
+            }
+            DisableSelection();
+        }
+
+        private void PivotChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Contains(PinnedPivot))
+            {
+                PinButton.Icon = new SymbolIcon(Symbol.UnPin);
+                PinButton.Label = "Unpin Selected";
+                PinButton.Click -= PinRoutes;
+                PinButton.Click += UnpinSelected;
+                loadPinned();
+            }else
+            {
+                PinButton.Icon = new SymbolIcon(Symbol.Pin);
+                PinButton.Label = "Pin Selected";
+                PinButton.Click -= UnpinSelected;
+                PinButton.Click += PinRoutes;
+            }
+            DisableSelection();
+        }
+
+        private void UnpinSelected(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = pinnedList.SelectedItems.OfType<RouteInfo>();
+            foreach (var item in selectedItems)
+            {
+                
+                var query = conn.Table<StopRoute>().Where(
+                    s => s.directionId == item.DirectionId && s.routeNum == item.RouteNum && s.stopId == item.StopId);
+                var sr = query.ElementAt(0);
+                var d = conn.Delete<StopRoute>(sr.id);
+                savedRoutes.Remove(item);
+            }
+            DisableSelection();
+        }
+
+        private void SearchBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            filtered.Clear();
+            if(sender.Text!="")
+                filtered = new ObservableCollection<string>(stopNames.Where(stopName => stopName.Contains(sender.Text.ToUpper())).Take(8));
+            sender.ItemsSource = filtered;
+        }
+
+        private void SearchBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            sender.Text = args.SelectedItem.ToString();
+        }
+
+        private void Select(object sender, RoutedEventArgs e)
+        {
+            ListView currentList;
+            if (rootPivot.SelectedItem == PinnedPivot)
+            {
+                currentList = pinnedList;
+            }else
+            {
+                currentList = resultsList;
+            }
+            if(currentList.SelectionMode == ListViewSelectionMode.None)
+            {
+                currentList.SelectionMode = ListViewSelectionMode.Multiple;
+                PinButton.IsEnabled = true;
+                menuBar.ClosedDisplayMode = AppBarClosedDisplayMode.Compact;
+            }else
+            {
+                currentList.SelectionMode = ListViewSelectionMode.None;
+                PinButton.IsEnabled = false;
+                menuBar.ClosedDisplayMode = AppBarClosedDisplayMode.Minimal;
+            }
+        }
+
+        private void DisableSelection(object sender, object e)
+        {
+            DisableSelection();
+        }
+
+        private void DisableSelection()
+        {
+            ListView currentList;
+            if (rootPivot.SelectedItem == PinnedPivot)
+            {
+                currentList = pinnedList;
+            }
+            else
+            {
+                currentList = resultsList;
+            }
+            currentList.SelectionMode = ListViewSelectionMode.None;
+            PinButton.IsEnabled = false;
+            menuBar.ClosedDisplayMode = AppBarClosedDisplayMode.Minimal;
+        }
+
+        private void Reload(object sender, RoutedEventArgs e)
+        {
+            if(rootPivot.SelectedItem == PinnedPivot)
+            {
+                loadPinned();
+            }else
+            {
+                SearchStop(SearchBox, null);
+            }
+        }
+    }
+}
---
+++ OC Bus Tracker/OCTranspoInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace OC_Bus_Tracker
+{
+    class OCTranspoInfo
+    {
+        public static string APP_ID = "a6ca2d89";
+        public static string API_KEY = "8bdf5b017c68e35354ef17259b7a4644";
+        public static string ROUTE_URL = "https://api.octranspo1.com/v1.2/GetNextTripsForStop";
+        public static string STOP_URL = "https://api.octranspo1.com/v1.2/GetNextTripsForStopAllRoutes";
+    }
+}
